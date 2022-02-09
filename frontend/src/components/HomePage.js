cea02b1475327cf939c86a6a664fabb751584080
Simon Bui <simonbui723@gmail.com>
2022-02-09 00:56:48 -0800

    part 9 of the tutorial
    -componentdidmount usage to join a room if you're already in one

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,24 +2,74 @@ import React, {Component} from 'react';
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from '@material-ui/core'
 import { BrowserRouter as Router, 
     Routes,
     Route, 
     Link, 
     Redirect,
+    Navigate,
 } from "react-router-dom";
 
 
 export default class HomePage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            roomCode: null,
+        }
+    }
+
+    async componentDidMount() {
+        fetch('/api/user-in-room')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    roomCode: data.code
+                });
+            });
+    }
+
+    renderHomePage() {
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h3" compact="h3">
+                        House Party
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <ButtonGroup disableElevation variant="contained" color="primary">
+                        <Button color="primary" to='/join' component={ Link }>
+                            Join a Room
+                        </Button>
+                        <Button color="secondary" to='/create' component={ Link }>
+                            Create a Room
+                        </Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
+        );
+    }
+
+    renderHomeOrJoin() {
+        console.log("wsuP");     
+        return this.state.roomCode ? (
+            <Navigate to={`/room/${this.state.roomCode}`} />
+          ) : (
+            this.renderHomePage()
+          );
     }
 
     render() {
         return (
         <Router>
             <Routes>
-                <Route path='/' element={<p>This is the home page</p>}/>
+                <Route 
+                    exact path='/' 
+                    element={this.renderHomeOrJoin()}
+                    />
+
                 <Route path='/join' element={<RoomJoinPage/>}/>
                 <Route path='/create' element={<CreateRoomPage/>}/>
                 <Route path='/room/:roomCode' element={<Room/>}/>
@@ -28,3 +78,4 @@ export default class HomePage extends Component {
         );
     }
 }
+
